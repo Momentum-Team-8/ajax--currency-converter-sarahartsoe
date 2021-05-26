f2767b61762acef31d70a14309346bde514f74bd
sarahartsoe <sarahartsoe@gmail.com>
2021-05-26 19:51:02 -0400

    two drop downs appearing but no connection.

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
 const currencies = [
-  'EUR',
-  'CAD',
-  'HKD',
-  'ISK',
-  'PHP',
-  'DKK',
-  'HUF',
-  'CZK',
-  'AUD',
-  'RON',
-  'SEK',
-  'IDR',
-  'INR',
-  'BRL',
-  'RUB',
-  'HRK',
-  'JPY',
-  'THB',
-  'CHF',
-  'SGD',
-  'PLN',
-  'BGN',
-  'TRY',
-  'CNY',
-  'NOK',
-  'NZD',
-  'ZAR',
-  'USD',
-  'MXN',
-  'ILS',
-  'GBP',
-  'KRW',
-  'MYR'
-]
+  "EUR",
+  "CAD",
+  "HKD",
+  "ISK",
+  "PHP",
+  "DKK",
+  "HUF",
+  "CZK",
+  "AUD",
+  "RON",
+  "SEK",
+  "IDR",
+  "INR",
+  "BRL",
+  "RUB",
+  "HRK",
+  "JPY",
+  "THB",
+  "CHF",
+  "SGD",
+  "PLN",
+  "BGN",
+  "TRY",
+  "CNY",
+  "NOK",
+  "NZD",
+  "ZAR",
+  "USD",
+  "MXN",
+  "ILS",
+  "GBP",
+  "KRW",
+  "MYR",
+];
 
-const url = "http://api.exchangeratesapi.io/v1/latest?access_key=62431ad0068809f046dd0088d9212cdd"
+const url =
+  "http://api.exchangeratesapi.io/v1/latest?access_key=62431ad0068809f046dd0088d9212cdd";
 
-const rootElement = document.querySelector('.container')
+const rootElement = document.querySelector(".container");
 
 // fetch(url)
 // // console.log(url)
@@ -59,22 +60,41 @@ const rootElement = document.querySelector('.container')
 // attempting Roan's method below:
 
 fetch(url)
-  .then(res => res.json()) 
-  .then(data => {
-    console.log(data)
-    let h2 = document.createElement('h2')
-    h2.innerText = data.base
-    rootElement.appendChild(h2)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    let h2 = document.createElement("h2");
+    h2.innerText = data.base;
+    rootElement.appendChild(h2);
 
-    let allCurrency = document.createElement('select')
-    rootElement.appendChild(allCurrency)
+    let allCurrency = document.createElement("select");
+    rootElement.appendChild(allCurrency);
     for (let cur of currencies) {
-      const optionItem = document.createElement('option')
-      optionItem.value = cur
-      optionItem.innerText = cur
-      allCurrency.appendChild(optionItem)
+      const optionItem = document.createElement("option");
+      optionItem.value = cur;
+      optionItem.innerText = cur;
+      allCurrency.appendChild(optionItem);
     }
-  })
+  });
 
-  //above is returning Uncaught (in promise) TypeError: Cannot read property 'appendChild' of null
-    // at script.js:67
+//above is returning Uncaught (in promise) TypeError: Cannot read property 'appendChild' of null
+// at script.js:67
+// debugged
+
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    let h2 = document.createElement("h2");
+    h2.innerText = data.base;
+    rootElement.appendChild(h2);
+
+    let allCurrency = document.createElement("select");
+    rootElement.appendChild(allCurrency);
+    for (let cur of currencies) {
+      const optionItem = document.createElement("option");
+      optionItem.value = cur;
+      optionItem.innerText = cur;
+      allCurrency.appendChild(optionItem);
+    }
+  });
